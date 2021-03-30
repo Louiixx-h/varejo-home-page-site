ae42d7c5d7c6454a59ff8589da269772515b20c3
Louiixx-h <louix.sm@gmail.com>
2021-03-30 01:38:08 -0300

    update slide and header

--- src/App.js
+++ src/App.js
@@ -3,43 +3,52 @@ import './App.css';
 import Slider from 'infinite-react-carousel';
 
 import carrinho from './carrinho.png'
+import conta from './conta.png'
 import logoipanema from './logo.png'
+
 import carnes from './carnes.png'
 import produtos from './produtos.png'
 import jogoPanelas from './jogo-panelas.png'
+import ferramentas from './ferramentas.png'
+import limpeza from './limpeza.png'
 
 function App() {
   return (
     <div className="App">
       <div className="header">
         <header className="App-header">
-          <img src={logoipanema} className="App-logo" alt="logo" width="200"/>
+          <a
+            href="/">
+            <img src={logoipanema} className="App-logo" alt="logo" width="200"/>
+          </a>
           <input className="search-header" 
             type="text" 
             name="search" 
             placeholder="Busque pelo seu produto."/>
+          <div>
           <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-          <img className="compras-header" src={carrinho} width="60"/>
+            href="/">
+            <img className="compras-header" src={conta} width="40"/>
           </a>
+          <a
+            href="/">
+            <img className="compras-header" src={carrinho} width="40"/>
+          </a>
+        </div>
         </header>
         <nav className="App-navigation">
           <div className="nav-buttons">
             <a href="/">
-              <p>Carnes</p>
+              <p>Elétrica</p>
             </a>
             <a href="/">
-              <p>Temperos</p>
+              <p>Cozinha</p>
             </a>
             <a href="/">
               <p>Casa</p>
             </a>
             <a href="/">
-            <p>Produtos de Limpeza</p>
+            <p>Calçados</p>
             </a>
             <a href="/">
               <p>Ferramentas</p>
@@ -54,7 +63,7 @@ function App() {
           duration={100}
           className="slider">
             <div>
-              <img src={carnes} width="100%"/>
+              <img src={ferramentas} width="100%"/>
             </div>
             <div>
               <img src={produtos} width="100%"/>
@@ -63,7 +72,7 @@ function App() {
               <img src={jogoPanelas} width="100%"/>
             </div>
             <div>
-              <img src={produtos} width="100%"/>
+              <img src={limpeza} width="100%"/>
             </div>
             <div>
               <img src={carnes} width="100%"/>
